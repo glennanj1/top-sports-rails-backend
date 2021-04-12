c5896623be8c0931dd547ee679ea8c66fdde7999
github username <github email address>
2021-04-12 18:29:24 -0400

    testing endpoint for nba conference

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,2 +1,20 @@
 class Conference < ApplicationRecord
+    def self.scraper 
+
+        url = URI("https://sportspage-feeds.p.rapidapi.com/conferences?league=NBA")
+
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+        request = Net::HTTP::Get.new(url)
+        request["x-rapidapi-key"] = ENV['KEY']
+        request["x-rapidapi-host"] = 'sportspage-feeds.p.rapidapi.com'
+
+        response = http.request(request)
+        data = response.read_body
+
+        binding.pry
+        Conference.new(data)
+    end
 end
