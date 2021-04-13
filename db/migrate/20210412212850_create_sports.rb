a50c1898a786f0400847dbf2bff6f8be11be7e5c
github username <github email address>
2021-04-13 11:20:41 -0400

    sports migration

---
+++ db/migrate/20210412212850_create_sports.rb
@@ -0,0 +1,13 @@
+class CreateSports < ActiveRecord::Migration[6.1]
+  def change
+    create_table :sports do |t|
+      t.string :key
+      t.boolean :active
+      t.string :group
+      t.string :details
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
