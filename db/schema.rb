b0d5694f518f9e65278c7abeca7573fa86ab3fad
github username <github email address>
2021-04-13 12:59:50 -0400

    added game migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_212850) do
+ActiveRecord::Schema.define(version: 2021_04_13_164043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "sport_nice"
+    t.string "teams"
+    t.string "home_team"
+    t.datetime "commence_time"
+    t.string "sites"
+    t.bigint "sport_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sport_id"], name: "index_games_on_sport_id"
+  end
 
   create_table "sports", force: :cascade do |t|
     t.string "key"
@@ -25,4 +37,5 @@ ActiveRecord::Schema.define(version: 2021_04_12_212850) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "games", "sports"
 end
