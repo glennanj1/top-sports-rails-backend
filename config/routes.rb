e6b68a0a73ce955a0f64f95d08479cfe5c25ccca
github username <github email address>
2021-04-14 13:33:15 -0400

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :games
-  resources :sports
+  resources :sports, only: [:index] do 
+    resources :games, shallow: true
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
