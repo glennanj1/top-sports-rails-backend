76794bfafa3fdbd0589b84c56678f21445a96950
github username <github email address>
2021-04-14 13:35:14 -0400

    removed pry and formated data from api endpoint

--- app/models/game.rb
+++ app/models/game.rb
@@ -15,10 +15,15 @@ class Game < ApplicationRecord
   
     data = response.read_body
     j = JSON[data]['data']
-    # j.map do |x|
-    #   teams
-    # end
 
-    binding.pry
+    j.map do |x|
+      sn = x['sport_nice']
+      t = "#{x['teams'][0]} vs. #{x['teams'][1]}"
+      ht = x['home_team']
+      ct = x['commence_time']
+      s = x['sites']
+      g = Game.new(sport_nice: sn, teams: t, home_team: ht, commence_time: ct, sites: s, sport_id: id)
+      g.save
+    end
   end
 end
