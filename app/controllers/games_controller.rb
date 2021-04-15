9edd4a78dc5541915c02cd6d60a1b382058f0691
glennanj1 <glennan20@gmail.com>
2021-04-15 14:35:23 -0400

    added show action for game odds

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,14 @@
 class GamesController < ApplicationController
     def index
         s = Sport.find_by(id: params[:sport_id])
-        Game.scraper(s.key, s.id)
+        # Game.scraper(s.key, s.id)
         games = s.games
         render json: GameSerializer.new(games)
+    end
+
+    def show 
+        g = Game.find(params[:id])
+        render json: GameSerializer.new(g)
     end
 
 end
