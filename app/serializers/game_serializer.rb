4999d4cc8da10f4dfcbc3c3dbeeb2b5e533f6d16
github username <github email address>
2021-04-17 15:41:30 -0400

    added odds to attributes

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,4 +1,4 @@
 class GameSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :sport_id, :sport_nice, :teams, :home_team, :commence_time, :sites
+  attributes :sport_id, :sport_nice, :teams, :home_team, :commence_time, :sites, :odds, :odds1
 end
