2dcd4e2dbb6f23c182e9e8df4af072660021107e
github username <github email address>
2021-04-19 19:38:30 -0400

    added team1 and team2 to game serializer

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,4 +1,4 @@
 class GameSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :sport_id, :sport_nice, :teams, :home_team, :commence_time, :sites, :odds, :odds1
+  attributes :sport_id, :sport_nice, :teams, :home_team, :commence_time, :sites, :odds, :odds1, :team_1, :team_2
 end
