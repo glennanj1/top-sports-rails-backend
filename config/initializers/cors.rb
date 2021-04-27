feb8cf79d032686f86aaca27498751efec292c09
github username <github email address>
2021-04-27 15:43:28 -0400

    added frontend in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://jolly-davinci-edd6d5.netlify.app'
     #change before deploying
 
     resource '*',
