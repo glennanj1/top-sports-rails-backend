ec49d3ee48c29e71c4f8ccdad04e50b5633e2087
github username <github email address>
2021-04-27 09:57:43 -0400

    uncommented game scraper

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 class GamesController < ApplicationController
     def index
         s = Sport.find_by(id: params[:sport_id])
-        # Game.scraper(s.key, s.id)
+        Game.scraper(s.key, s.id)
         games = s.games
         render json: GameSerializer.new(games)
     end
